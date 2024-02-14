9dfe759ed0027dd177491cc2eb4759af643f8b4f
Europaprojekt <159050628+Europaprojekt@users.noreply.github.com>
2024-02-14 20:14:23 +0100

    Added JB Mono font in extraBold & extraBoldItalic Customized sizing of the currently existing header

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,32 @@ var currentPage = ref("home")
 <style lang="scss">
 
     .App {
+        $headerHeight: max(100px, 10dvh);
 
         &__header {
             position: fixed;
+            justify-content: center;
+            display: flex;
             top: 0;
             left: 0;
             background: white;
             width: 100%;
-            height: 100px;
+            height: $headerHeight;
 
-            color: purple;
+            & h1 {
+                font-size: min(8dvw, calc($headerHeight * .6));
+                color: #0042a4;
+                align-self: center;
+            }
         }
 
         &__main {
-            margin-block-start: 100px;
+            margin-block-start: $headerHeight;
         }
 
         &__bgImg {
             height: auto;
-            max-height: calc(100dvh - 100px);
+            max-height: calc(100dvh - $headerHeight);
             width: 100%;
             object-position: center;
             object-fit: cover;
